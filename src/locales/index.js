36ab41b496a8bb9aa362445f5f4fcd33e849ba65
Andriy Tsaryov <andriy.tsaryov@eleken.co>
2019-01-16 15:20:59 +0000

    Merged in feature/HP-298-add-localize (pull request #105)
    Feature/HP-298 add localize
    
    Approved-by: Pavel Maksimovich <pavel.maksimovich@eleken.co>

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,13 +2,16 @@ import { addLocaleData } from 'react-intl'
 
 import enLocaleData from 'react-intl/locale-data/en'
 import esLocaleData from 'react-intl/locale-data/es'
+import ptLocaleData from 'react-intl/locale-data/pt'
 
-import en from './en'
-import es from './es'
+import en from './en.json'
+import es from './es.json'
+import pt from './pt.json'
 
-addLocaleData([...enLocaleData, ...esLocaleData])
+addLocaleData([...enLocaleData, ...esLocaleData, ...ptLocaleData])
 
 export default {
   en,
   es,
+  pt,
 }
