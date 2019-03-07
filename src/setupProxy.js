da0dbff3f9925a5cb96f4e68b7e4d6a61189ce98
Andriy Tsaryov <andriy.tsaryov@eleken.co>
2019-03-07 09:24:08 +0000

    setupProxy.js edited online with Bitbucket

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,15 @@
 const proxy = require('http-proxy-middleware')
 
-const { REACT_APP_API_BASE_URL } = process.env
+const { REACT_APP_API_BASE_URL, USE_NGINX_PROXY } = process.env
 
 module.exports = app => {
-  app.use(
-    proxy('/api', {
-      changeOrigin: true,
-      secure: false,
-      target: REACT_APP_API_BASE_URL,
-    }),
-  )
+  if (!USE_NGINX_PROXY) {
+    app.use(
+      proxy('/api', {
+        changeOrigin: true,
+        secure: false,
+        target: REACT_APP_API_BASE_URL,
+      }),
+    )
+  }
 }
