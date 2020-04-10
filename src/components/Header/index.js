fd931a8159ea7844667d1cbd9f90e91c3333e5f2
Oleksandr Prokopenko <oleksandr.prokopenko@kalon.tech>
2020-04-10 18:52:19 +0300

    created styled file for header component, moved styled components to styled file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,13 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link, withRouter } from 'react-router-dom'
 import { compose, bindActionCreators } from 'redux'
-import { Layout, Menu, Popover, Affix, Button, Alert, Icon } from 'antd'
+import { Menu, Popover, Alert, Icon } from 'antd'
 import { FormattedMessage } from 'react-intl'
-import styled, { css } from 'styled-components'
 import { connect } from 'react-redux'
 import { Animate } from 'react-animate-mount'
 import moment from 'moment'
+
+import { sizes } from 'utils/mediaQueryTemplate'
 
 import ExpandMoreIcon from 'assets/icons/ExpandMoreIcon'
 import FingerPrintIcon from 'assets/icons/FingerPrintIcon'
@@ -15,7 +16,6 @@ import MenuIcon from 'assets/icons/MenuIcon'
 import CloseIcon from 'assets/icons/CloseIcon'
 import HeaderLanguageSelector from 'components/HeaderLanguageSelector'
 import CollapseLanguageSelector from 'components/CollapseLanguageSelector'
-import CollapsedMenu from 'components/CollapsedMenu'
 import NotificationsContainer from 'components/NotificationsContainer'
 import {
   PrimaryButton,
@@ -26,8 +26,7 @@ import {
 
 import colors from 'config/colors'
 import { getBrandedConfig } from 'config/branded'
-import media, { sizes } from 'utils/mediaQueryTemplate'
-import hexToRgba from 'utils/hexToRgba'
+
 import { logOut } from 'redux/accountStore'
 import { getUserInitialAvatar } from 'api'
 import * as apiActions from 'api/actions'
@@ -42,517 +41,48 @@ import newsBellIcon from './assets/newsBellIcon.svg'
 import { ReactComponent as Atom } from '../../assets/unit-icons/atom.svg'
 import { ReactComponent as Clock } from '../../assets/unit-icons/clock.svg'
 
+import {
+  LeftAlignPublic,
+  RightAlign,
+  CenterAlign,
+  LogoImg,
+  ImgLeftIndent,
+  ImgRightIndent,
+  HeaderWrap,
+  LeftMenu,
+  CenterMenu,
+  RightMenu,
+  Logo,
+  LogoSmall,
+  MenuWrap,
+  Avatar,
+  Name,
+  Email,
+  Links,
+  UserInfo,
+  Hamburger,
+  CollapseContent,
+  CollapseMenu,
+  CollapseSubmenuTitle,
+  CollapseTop,
+  ProfileMenu,
+  ProfileSettingsPopover,
+  BlueBorderedButton,
+  GrayBorderedButton,
+  NotificationCount,
+  NotificationPopoverTitle,
+  TakeActionButton,
+  StyledNotificationsPopover,
+  StyledAffix,
+  UnitsBlock,
+  SVGClockWrap,
+  SVGAtomWrap,
+} from './styled'
+
 // import { UIContextSettings } from '../../context/uiSettingsContext'
 // const UIContextData = useContext(UIContextSettings)
 
 const SubMenu = Menu.SubMenu
-
-const LeftAlignPublic = styled.div`
-  margin-right: 34px;
-`
-
-const RightAlign = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  flex: 1;
-`
-const CenterAlign = styled.div`
-  display: flex;
-  align-items: center;
-`
-const LogoImg = styled.img`
-  margin-right: 10px;
-`
-const ImgRightIndent = styled.img`
-  margin-right: 5px;
-`
-const ImgLeftIndent = styled.img`
-  margin-left: 5px;
-`
-
-const HeaderWrap = styled(Layout.Header)`
-  position: relative;
-  z-index: 970;
-  background: ${colors.white};
-  height: 90px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 16px;
-  font-family: ${({ font }) => font || '"Noto Sans", sans-serif'};
-  ${media.largeDesktop`
-    padding: 0 34px;
-  `}
-  ${media.phone`
-    height: 70px;
-    padding: 0 15px;
-  `}
-  .ant-menu-item {
-    padding: 0;
-    border-bottom: 3px solid transparent;
-    font-size: 16px;
-    a {
-      color: ${({ fontColor }) => fontColor || colors.darkGray};
-    }
-
-    &:hover,
-    &.ant-menu-item-active {
-      color: ${hexToRgba(colors.dark, 0.8)};
-      border-bottom: 3px solid transparent;
-      a {
-        color: inherit;
-      }
-    }
-
-    &.ant-menu-item-selected {
-      border-color: inherit;
-      color: ${colors.dark};
-
-      ${props =>
-        !props.isLoggedIn &&
-        css`
-          border-bottom: 3px solid transparent;
-        `}
-
-      a {
-        color: inherit;
-      }
-    }
-  }
-
-  .ant-menu {
-    border: none;
-  }
-`
-
-const LeftMenu = styled.div`
-  display: flex;
-  align-items: center;
-
-  .ant-menu-item {
-    margin-right: 34px;
-    line-height: 85px;
-  }
-`
-
-export const CenterMenu = styled(LeftMenu)`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  .ant-menu-item {
-    margin-right: 40px;
-  }
-  .ant-menu-horizontal {
-    border-color: ${({ borderColor }) => borderColor || colors.green};
-  }
-`
-
-const RightMenu = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-
-  a {
-    display: inline-flex;
-    margin-left: 35px;
-  }
-
-  .ant-button {
-    border-radius: 6px;
-  }
-`
-
-const Logo = styled.div`
-  margin-right: 40px;
-`
-
-const LogoSmall = styled.div`
-  display: block;
-  flex: 1;
-  justify-content: flex-start;
-  ${media.largeDesktop`
-    justify-content: center;
-    a {
-      display: flex;
-      justify-content: center;
-    }
-  `}
-`
-
-const MenuWrap = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-
-  .ant-menu-horizontal {
-    border-color: ${({ borderColor }) => borderColor || colors.green};
-  }
-`
-
-const Avatar = styled.div`
-  height: 42px;
-  width: 42px;
-  box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.1);
-  border-radius: 50px;
-  background: ${colors.white};
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: ${colors.green};
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-  }
-`
-
-const Name = styled.p`
-  font-size: 19px;
-  line-height: 27px;
-  color: inherit;
-`
-
-const Email = styled.p`
-  color: ${colors.darkGray};
-`
-
-const Links = styled.div`
-  position: relative;
-  margin-top: 37px;
-  &:before {
-    position: absolute;
-    top: -20px;
-    left: -10px;
-    right: -10px;
-    content: '';
-    height: 1px;
-    background: ${hexToRgba(colors.dark, 0.17)};
-  }
-  a {
-    display: block;
-    margin-bottom: 10px;
-    font-size: 16px;
-    line-height: 20px;
-  }
-`
-const UserInfo = styled.div`
-  padding: 8px 4px;
-  color: ${({ fontColor }) => fontColor || colors.darkGray};
-`
-
-const Hamburger = styled.div`
-  display: flex;
-  align-items: center;
-  color: ${colors.darkGray};
-  cursor: pointer;
-  transition: all 0.3s;
-  &:hover {
-    color: ${colors.dark};
-  }
-`
-
-const CollapseMenu = styled(CollapsedMenu)`
-  z-index: 2;
-  position: fixed;
-  top: 89px;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background: ${colors.white};
-  color: ${colors.dark};
-  overflow: auto;
-  ${media.phone`
-    top: 70px;
-  `}
-`
-
-const CollapseTop = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 35px 80px;
-  background: ${colors.lightGray};
-
-  ${media.phone`
-    flex-direction: column;
-    justify-content: center;
-    padding: 35px 15px;
-  `}
-  a {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    margin-right: 15px;
-    &:last-child {
-      margin-right: 0;
-      margin-bottom: 0;
-    }
-    .ant-btn {
-      width: 100%;
-    }
-    ${media.phone`
-      margin-bottom: 10px;
-      margin-right: 0;
-    `}
-  }
-`
-const CollapseSubmenuTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  font-size: 16px;
-  padding: 0 45px;
-  ${media.phone`
-    padding: 0 15px;
-  `}
-`
-
-const CollapseContent = styled.div`
-  padding: 0 35px;
-  .ant-menu-inline {
-    border: none;
-    .ant-menu-item {
-      padding-left: 0;
-      margin: 0;
-      padding-right: 0;
-      border-bottom: 1px solid ${hexToRgba(colors.dark, 0.17)};
-      height: auto;
-      font-size: 16px;
-      color: ${colors.dark};
-      &.ant-menu-item-active,
-      &.ant-menu-item-selected {
-        background: none;
-        color: ${colors.dark};
-        &:after {
-          display: none;
-        }
-      }
-      a {
-        padding: 0 45px;
-        line-height: 80px;
-        white-space: nowrap;
-        color: inherit;
-        ${media.phone`
-          padding: 0 15px;
-        `}
-      }
-    }
-
-    .ant-menu-submenu {
-      padding: 20px 0;
-      border-bottom: 1px solid ${hexToRgba(colors.dark, 0.17)};
-      .ant-menu-submenu-title {
-        height: auto;
-      }
-      .ant-menu-item {
-        border: none;
-        height: auto;
-        margin-top: 25px;
-        line-height: normal;
-        color: ${colors.green};
-        a {
-          padding-left: 105px;
-          line-height: normal;
-          color: inherit;
-          ${media.phone`
-            padding-left: 30px;
-          `}
-        }
-      }
-    }
-
-    .ant-menu-submenu-arrow {
-      display: none;
-    }
-
-    .ant-menu-submenu-open {
-      .anticon {
-        transform: rotate(-180deg);
-      }
-    }
-
-    .anticon {
-      margin-right: -15px;
-      transition: transform 0.3s;
-      ${media.phone`
-       margin-right: 0;
-      `}
-    }
-
-    .ant-menu-submenu-title {
-      padding-left: 0;
-      padding-right: 0;
-      margin: 0;
-      display: flex;
-      overflow: visible;
-      color: ${colors.dark};
-    }
-  }
-  ${media.phone`
-    padding: 0 15px;
-  `}
-`
-
-const ProfileSettingsPopover = styled(Popover)`
-  .ant-popover-content .ant-popover-arrow {
-    top: 3px;
-  }
-`
-
-const ProfileMenu = styled.div`
-  color: inherit;
-
-  .menu-item__user-data {
-    flex-grow: 1;
-  }
-
-  .menu-item__user-data > p:last-child {
-    max-width: 100%;
-    white-space: normal;
-    word-break: break-all;
-  }
-
-  div${Avatar} {
-    margin-right: 15px;
-  }
-
-  p${Name} {
-    color: ${colors.dark};
-  }
-`
-
-const StyledAffix = styled(Affix)`
-  .ant-affix {
-    z-index: 970;
-    width: 100% !important;
-    box-shadow: 0 1px 10px 0 ${hexToRgba(colors.dark, 0.08)};
-  }
-`
-
-const BlueBorderedButton = styled(Button)`
-  border: 2px solid ${colors.darkBlue};
-  background: transparent;
-  border-radius: 0;
-  color: ${colors.darkBlue};
-  &&:hover,
-  &&:focus {
-    border-color: ${colors.darkBlue};
-    background: ${colors.darkBlue};
-    color: ${colors.white};
-  }
-`
-
-const GrayBorderedButton = styled(Button)`
-  border: 1px solid ${colors.interfaceFooterColor2};
-  background: transparent;
-  border-radius: 2px;
-  color: ${colors.interfaceFooterColor2};
-  &&:hover,
-  &&:focus {
-    border-color: ${colors.interfaceFooterColor2};
-    background: ${colors.interfaceFooterColor2};
-    color: ${colors.white};
-  }
-`
-
-const StyledNotificationsPopover = styled(Popover)`
-  .ant-popover-inner-content {
-    padding: 0;
-  }
-  .ant-popover-content .ant-popover-arrow {
-    top: 3px;
-    background-color: ${colors.green};
-    border-color: ${colors.green};
-  }
-`
-
-const NotificationPopoverTitle = styled.div`
-  display: flex;
-  position: relative;
-  justify-content: center;
-  margin-right: 17px;
-  width: 50px;
-  ${media.largeDesktop`
-    margin-right: 0;
-  `}
-  img {
-    cursor: pointer;
-    opacity: 0.4;
-  }
-`
-
-const NotificationCount = styled.div`
-  width: 15px;
-  height: 15px;
-  position: absolute;
-  top: 5px;
-  right: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  background-color: #e67424;
-  font-family: ${({ fontNames }) => fontNames || '"Noto Sans", sans-serif'};
-  font-weight: bold;
-  line-height: 10px;
-  font-size: 10px;
-  color: ${colors.white};
-
-  ${media.largeDesktop`
-    top: -5px;
-  `}
-`
-
-const TakeActionButton = styled(PrimaryButton)`
-  color: ${colors.white};
-  background-color: ${colors.green};
-  min-width: 140px;
-  margin-left: 25px;
-  font-size: 14px;
-  &:focus,
-  &:active,
-  &:hover {
-    color: ${colors.white};
-  }
-  ${media.desktop`
-    margin-left: 0px;
-    min-width: 274px;
-  `}
-`
-
-const UnitsBlock = styled.div`
-  position: absolute;
-  top: 34px;
-  right: 176px;
-  width: 44px;
-  height: 22px;
-  border-radius: 50px;
-  background-color: lightgray;
-  margin-right: 31px;
-`
-
-const SVGAtomWrap = styled.div`
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  path {
-    fill: ${props => props.color};
-  }
-`
-
-const SVGClockWrap = styled.div`
-  position: absolute;
-  top: 2px;
-  right: 20px;
-  path {
-    fill: ${props => props.color};
-  }
-`
 
 const isIE =
   navigator.userAgent.indexOf('MSIE') !== -1 ||
