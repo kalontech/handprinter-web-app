66a6154fe97f9aab53f1f13d50307fcc39d03c3c
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2020-04-28 13:14:58 +0300

    Fix

--- src/pages/CampaignDashboard/participants.js
+++ src/pages/CampaignDashboard/participants.js
@@ -30,7 +30,6 @@ export default function renderParticipants(props) {
           i => i.accomplishedActions.length >= actionsNumberToComplete,
         )
       : participants
-  console.log(participantsFiltered)
   return (
     <Fragment>
       <MenuStyled
