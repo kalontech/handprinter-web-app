4d2bf4bb0a85aaa609ccfb4c5da66dc42e3db704
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2020-10-29 15:15:02 +0200

    Switch to localStorage instead of cookies

--- src/config/persist.js
+++ src/config/persist.js
@@ -1,5 +1,6 @@
 import { CookieStorage } from 'redux-persist-cookie-storage'
 import Cookies from 'cookies-js'
+import storage from 'redux-persist/lib/storage'
 
 import env from 'config/env'
 
@@ -8,22 +9,22 @@ Cookies.defaults.domain = env.REACT_APP_WEB_APP_COOKIES_DOMAIN
 const persistConfig = {
   user: {
     key: 'handprinter_user',
-    storage: new CookieStorage(Cookies),
+    storage,
     whitelist: [],
   },
   account: {
     key: 'handprinter_account',
-    storage: new CookieStorage(Cookies),
+    storage,
     whitelist: ['token'],
   },
   app: {
     key: 'handprinter_app',
-    storage: new CookieStorage(Cookies),
+    storage,
     whitelist: [],
   },
   intl: {
     key: 'handprinter_intl',
-    storage: new CookieStorage(Cookies),
+    storage,
     whitelist: ['locale'],
   },
 }
